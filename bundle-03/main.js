0e566220e551b55191e101d377ebc6d0167738d4
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-12-09 17:14:17 +0100

    bundle 3 fatto

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -84,3 +84,85 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+//Risposte
+/**
+ 1) L'esercizio verifica le proprietà degli oggetti contenuti nell'array cars e li divide in tre liste: auto a benzina, auto a diesel e tutte le altre
+ 2) Sì, ci sono errori di sintassi (come virgole mancanti, arrow functions scritte male, return mancanti)
+ 3) Sì, ci sono errori di logica (controlli impostati male senza .toLowerCase() e con operatori logici sbagliati (or al posto di and)); chiamerei inoltre 'car' e non 'auto' all'interno dei filter per coerenza.
+
+ const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    },
+];
+
+const gasolineCars = cars.filter( (car) => car.type.toLowerCase() === 'benzina');
+
+const dieselCars = cars.filter( (car) => {
+    return car.type.toLowerCase() === 'diesel';
+});
+
+const otherCars = cars.filter( (car) => {
+    return car.type.toLowerCase() !== 'benzina' && car.type.toLowerCase() !== 'diesel';
+});
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
+
+ */
